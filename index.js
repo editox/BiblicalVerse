499d070d7c52753293e49b0a39c3a1aca39b45d1
Mayk Perez <edisonperez1995@gmail.com>
2020-09-09 09:39:41 -0500

    deleting some things

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// const title = document.getElementById("title").checked;
-// const content = document.getElementById("content").checked;
-
 const switchContainer = document.getElementById("switch");
-const switchContent = document.getElementById("content");
-
 const container = document.querySelector('.container');
 
-
-
-// switchTitle.addEventListener('change', () => title.style.backgroundColor = '#2b2b2b');
 switchContainer.addEventListener('change', cardRotate);
-
 
 //cambiando una clase
 function cardRotate() {
